848a3618e383334d6129bcc3e6ca42854d39b39d
TomasOtano25 <tomasotano25@gmail.com>
2018-06-16 14:19:35 -0400

    variables

---
+++ variables.rb
@@ -0,0 +1,10 @@
+character_name = "John"
+character_age  = "35"
+
+puts "There once was a man named " + character_name
+puts "He was " + character_age  + " years old."
+
+character_name = "Mike"
+
+puts ("he really  liked the name " + character_name)
+puts ("But didn't like being " + character_age)
